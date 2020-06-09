d6b3c2cd11dbbdfe4bcb2b82545bc3253bf1dd20
lourod1987 <luisrodrigueziii.dev@gmail.com>
2020-06-09 19:42:25 -0400

    Feat - set up initial component and styles for Playlist

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Playlist extends React.Component {
+  render() {
+    return (
+      <div class="Playlist">
+        <input value="New Playlist"/>
+        <!-- Add a TrackList component -->
+        <button class="Playlist-save">SAVE TO SPOTIFY</button>
+      </div>
+    );
+  }
+}
