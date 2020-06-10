9e6b078f49e8ba1e773cfafc788dcb783cd286f0
lourod1987 <luisrodrigueziii.dev@gmail.com>
2020-06-09 20:21:40 -0400

    Style - adjust spacing of code

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -3,16 +3,16 @@ import Track from '../Track/Track';
 import './TrackList.css';
 
 class TrackList extends React.Component {
-    render() {
-        return (
-            <div className="TrackList">
-                <!-- You will add a map method that renders a set of Track components  -->
-                <Track />
-                <Track />
-                <Track />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="TrackList">
+        <!-- You will add a map method that renders a set of Track components  -->
+        <Track />
+        <Track />
+        <Track />
+      </div>
+    )
+  }
 }
 
 export default TrackList;
