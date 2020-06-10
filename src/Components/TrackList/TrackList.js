449e0f63e133ca406b5cab25a05ae651f8b5c779
lourod1987 <luisrodrigueziii.dev@gmail.com>
2020-06-09 20:17:17 -0400

    Fix - add in export statement, import styles and needed component

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Track from '../Track/Track';
+import './TrackList.css';
 
 class TrackList extends React.Component {
     render() {
         return (
             <div className="TrackList">
                 <!-- You will add a map method that renders a set of Track components  -->
+                <Track />
             </div>
         )
     }
 }
+
+export default TrackList;
