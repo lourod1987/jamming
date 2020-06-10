6ab4ee4c539e9e9ef6e65046f323122248fca434
lourod1987 <luisrodrigueziii.dev@gmail.com>
2020-06-10 02:15:03 -0400

    Feat - add in JS prop keys to html

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -16,11 +16,12 @@ class Track extends React.Component {
   }
 
   render() {
+    const { track } = this.props;
     return (
       <div className="Track">
         <div className="Track-information">
-          <h3><!-- track name will go here --></h3>
-          <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+          <h3>{track.name}</h3>
+          <p>{track.artist} | {track.album}</p>
         </div>
         <button className="Track-action"><!-- + or - will go here --></button>
       </div>
