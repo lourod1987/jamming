822961c04b0462d42a88cdaf19d0f1cddb2e25d8
lourod1987 <luisrodrigueziii.dev@gmail.com>
2020-06-09 20:09:16 -0400

    Fix - change class to className to comply with JSX requirements

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -5,10 +5,10 @@ import './Playlist.css';
 class Playlist extends React.Component {
   render() {
     return (
-      <div class="Playlist">
+      <div className="Playlist">
         <input value="New Playlist"/>
         <TrackList />
-        <button class="Playlist-save">SAVE TO SPOTIFY</button>
+        <button className="Playlist-save">SAVE TO SPOTIFY</button>
       </div>
     );
   }
