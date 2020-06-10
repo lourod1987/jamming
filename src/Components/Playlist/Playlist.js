491a62fa0b75cba2a08be4636269cdeb54e17405
lourod1987 <luisrodrigueziii.dev@gmail.com>
2020-06-09 20:08:02 -0400

    Fix - add in export statement, import styles and needed component

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import TrackList from '../TrackList/TrackList';
+import './Playlist.css';
 
 class Playlist extends React.Component {
   render() {
     return (
       <div class="Playlist">
         <input value="New Playlist"/>
-        <!-- Add a TrackList component -->
+        <TrackList />
         <button class="Playlist-save">SAVE TO SPOTIFY</button>
       </div>
     );
   }
 }
+
+export default TrackList;
