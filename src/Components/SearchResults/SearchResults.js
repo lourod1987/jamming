39ac6c02251382843a50e1a30189d70c7faa2bca
lourod1987 <luisrodrigueziii.dev@gmail.com>
2020-06-09 20:01:43 -0400

    Style - adjusting spacing of code

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -3,14 +3,14 @@ import TrackList from '../TrackList/TrackList';
 import './SearchResults.css';
 
 class SearchResults extends React.Component {
-    render() {
-        return (
-            <div className="SearchResults">
-                <h2>Results</h2>
-								<TrackList />
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div className="SearchResults">
+				<h2>Results</h2>
+				<TrackList />
+			</div>
+		)
+	}
 }
 
 export default SearchResults;
