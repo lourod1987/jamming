29d1012f02e1e6920bf8dfc5d708c4d3c6b78fa1
lourod1987 <luisrodrigueziii.dev@gmail.com>
2020-06-10 01:37:13 -0400

    Feat - importing necessary components, settings up stateful component and state, passing down properties

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,20 +1,37 @@
 import React from 'react';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
-      <div className="App">
-        <!-- Add a SearchBar component -->
-        <div className="App-playlist">
-          <!-- Add a SearchResults component -->
-          <!-- Add a Playlist component -->
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchResults: [
+        { name: 'still feel', artist: 'half alive', album: 'something' },
+        { name: 'getaway car', artist: 'Audioslave', album: 'Audioslave' },
+        { name: 'ordinary people', artist: 'John Legend', album: 'Living' },
+      ],
+    }
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} />
+            <Playlist />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
