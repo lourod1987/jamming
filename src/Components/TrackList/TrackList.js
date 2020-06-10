0f793d995ec15bc45e51369b8893f2fcf145bc25
lourod1987 <luisrodrigueziii.dev@gmail.com>
2020-06-10 02:10:57 -0400

    Feat - set up map to create track components rendered based off of searchResults prop

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -6,10 +6,7 @@ class TrackList extends React.Component {
   render() {
     return (
       <div className="TrackList">
-        <!-- You will add a map method that renders a set of Track components  -->
-        <Track />
-        <Track />
-        <Track />
+        this.props.tracks.map( track => <Track key={track.id} track={track} />);
       </div>
     )
   }
