ae0b460e9082679f474f503ae071c78a3aa50bbc
lourod1987 <luisrodrigueziii.dev@gmail.com>
2020-06-11 22:49:36 -0400

    Feat - passing down remove track props to Track component

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -12,6 +12,8 @@ class TrackList extends React.Component {
             key={track.id} 
             track={track}
             onAdd={this.props.onAdd}
+            onRemove={this.props.onRemove}
+            isRemoval={this.props.isRemoval}
           />
           );
       })}
