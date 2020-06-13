990cf51a21a2409484252a01aff106a5986caa25
lourod1987 <luisrodrigueziii.dev@gmail.com>
2020-06-12 22:47:25 -0400

    Feat - add handleNameChange method to pass in input's value to onNameChange method

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -3,10 +3,19 @@ import TrackList from '../TrackList/TrackList';
 import './Playlist.css';
 
 class Playlist extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+
+  handleNameChange(e) {
+    this.props.onNameChange(e);
+  }
+
   render() {
     return (
       <div className="Playlist">
-        <input defaultValue={'New Playlist'} />
+        <input defaultValue={'New Playlist'}  onChange={ e => this.handleNameChange(e.target.value)}/>
         <TrackList 
           tracks={this.props.playlistTracks}
           onRemove={this.props.onRemove}
