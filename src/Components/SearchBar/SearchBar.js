b7b10533933d806e7bffa6973bc242fb422a4c83
lourod1987 <luisrodrigueziii.dev@gmail.com>
2020-06-14 12:44:48 -0400

    Feat - add searchTerm and handleTermChange methods, track search term changes via event handler on input element

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,10 +2,29 @@ import React from 'react';
 import './SearchBar.css';
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    state = {
+      searchTerm: ''
+    }
+    this.search = this.search.bind(this);
+    this.handleTermChange = this.handleTermChange.bind(this);
+  }
+
+  search(searchTerm) {
+    this.props.onSearch(searchTerm)
+  }
+
+  handleTermChange(e) {
+    this.setState({
+      searchTerm: e
+    });
+  }
+
   render() {
     return (
       <div className="SearchBar">
-        <input placeholder="Enter A Song, Album, or Artist" />
+        <input placeholder="Enter A Song, Album, or Artist" onChange={e => this.handleTermChange(e.target.value)} />
         <button className="SearchButton">SEARCH</button>
       </div>
     );
